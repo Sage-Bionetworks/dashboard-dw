c6f73e2d658acc5e46fa8c9f33354c234ff42f1a
Kimyen <kimyen0816@gmail.com>
2014-10-14 11:03:36 -0700

    add postgresql to bootstrap.sh

--- vagrant-scripts/bootstrap.sh
+++ vagrant-scripts/bootstrap.sh
@@ -12,6 +12,15 @@ apt-get --quiet --yes --target-release wheezy-backports upgrade
 # NFS Client
 apt-get --quiet --yes --target-release wheezy-backports install nfs-common
 
+# PostgreSQL
+apt-get --quiet --yes --target-release wheezy-backports install postgresql
+apt-get --quiet --yes --target-release wheezy-backports install postgresql-client
+echo "listen_addresses = '*'" >> /etc/postgresql/9.1/main/postgresql.conf
+echo "host all all 10.0.0.0/16 trust" >> /etc/postgresql/9.1/main/pg_hba.conf
+su - postgres -c "psql -f /vagrant/dw-bootstrap.sql"
+su - vagrant
+service postgresql restart
+
 # Java
 apt-get --quiet --yes --target-release wheezy-backports install openjdk-7-jdk
 

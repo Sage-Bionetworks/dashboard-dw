5c99e36dcf78f776a9180331ff92fef4a60ac1c1
Kimyen <kimyen0816@gmail.com>
2014-10-16 09:48:36 -0700

    remove unused codes

--- src/main/java/org/sagebionetworks/dashboard/config/CommandArgsPropertyReader.java
+++
@@ -1,8 +0,0 @@
-package org.sagebionetworks.dashboard.config;
-
-public class CommandArgsPropertyReader implements PropertyReader {
-    @Override
-    public String read(final String key) {
-        return System.getProperty(key);
-    }
-}

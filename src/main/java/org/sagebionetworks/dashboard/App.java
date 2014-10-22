d9522757ccc6b29fe8a0321d70ad8f012960a504
Kimyen <kimyen0816@gmail.com>
2014-10-22 10:19:43 -0700

    add code to run scheduler

--- src/main/java/org/sagebionetworks/dashboard/App.java
+++ src/main/java/org/sagebionetworks/dashboard/App.java
@@ -26,16 +26,19 @@ public class App {
             throw new IllegalArgumentException("File " + filePath.getPath() + " does not exist.");
         }
 
-        /*if (args[1]=="true" || args[1]=="TRUE" || args[1]=="True") {
+        final Logger logger = org.slf4j.LoggerFactory.getLogger(App.class);
+        final boolean isProd = Boolean.parseBoolean(args[1]);
+        logger.info("Prod = " + isProd);
+        if (isProd) {
+
             @SuppressWarnings("resource")
             final ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/spring/scheduler-context.xml");
             context.registerShutdownHook();
             context.start();
-        } else */{
+        } else {
             final ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/spring/app-context.xml");
             context.registerShutdownHook();
-            final Logger logger = org.slf4j.LoggerFactory.getLogger(App.class);
-    
+
             final List<File> files = new ArrayList<File>();
             getCsvGzFiles(filePath, files);
             final int total = files.size();

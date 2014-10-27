b7bcd31d71a78edce87d32acb23b28aace57c6c5
Kimyen <kimyen0816@gmail.com>
2014-10-27 13:52:18 -0700

    update the table schema

--- src/main/resources/META-INF/spring/LogFileTable.sql
+++ src/main/resources/META-INF/spring/LogFileTable.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS log_file (
-    id          varchar(100)    PRIMARY KEY,
+    id          char(37)     NOT NULL,
     file_path   varchar(100)    NOT NULL,
-    log_type    integer         NOT NULL
+    log_type    integer         NOT NULL,
+    PRIMARY KEY (file_path, log_type)
 );
 
 CREATE INDEX ON log_file USING btree(id);
-
-CREATE INDEX ON log_file USING btree(log_type);

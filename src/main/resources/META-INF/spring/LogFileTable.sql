5a95bc763f17659ddde57b4e87c44ffe7fffa1aa
Kimyen <kimyen0816@gmail.com>
2014-11-05 14:35:07 -0800

    change the schema and add transactional annotation

--- src/main/resources/META-INF/spring/LogFileTable.sql
+++ src/main/resources/META-INF/spring/LogFileTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS log_file (
     id          char(37)        UNIQUE,
-    file_path   varchar(100)    NOT NULL,
+    file_path   text            NOT NULL,
     log_type    integer         NOT NULL,
     status      varchar(10)     NOT NULL,
     PRIMARY KEY (file_path, log_type)

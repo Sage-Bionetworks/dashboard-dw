dd88591f604aec05eee6d6e53ea7844149c9a322
Kimyen <kimyen0816@gmail.com>
2014-11-10 13:42:08 -0800

    check if the file is already copied

--- src/main/java/org/sagebionetworks/dashboard/service/RawAccessRecordService.java
+++ src/main/java/org/sagebionetworks/dashboard/service/RawAccessRecordService.java
@@ -1,7 +1,10 @@
 package org.sagebionetworks.dashboard.service;
 
+import java.util.UUID;
+
 import javax.annotation.Resource;
 
+import org.sagebionetworks.dashboard.dao.LogFileDao;
 import org.sagebionetworks.dashboard.dao.RawAccessRecordDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,13 +16,27 @@ public class RawAccessRecordService {
     private final Logger logger = LoggerFactory.getLogger(RawAccessRecordService.class);
 
     @Resource
+    private LogFileDao logFileDao;
+
+    @Resource
     private RawAccessRecordDao rawAccessRecordDao;
 
     public void update(final String filePath, final String username, final String password) {
+        if (logFileDao.isCompleted(filePath)) {
+            return;
+        }
+        String id = UUID.randomUUID().toString();
         try {
+            // type 0 for access_record
+            logFileDao.put(filePath, id, 0);
             rawAccessRecordDao.copy(filePath, username, password);
+            logFileDao.update(id);
         } catch (Throwable exception) {
-            logger.error("Failed to copy file " + filePath);
+            // if it's not a duplicate file, log the error
+            if (!exception.getMessage().contains("already exists")) {
+                logger.error("Failed to copy file " + filePath);
+            }
+            return;
         }
     }
 }

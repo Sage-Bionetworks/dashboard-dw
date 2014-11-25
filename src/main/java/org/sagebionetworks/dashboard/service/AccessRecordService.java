ceb3884a028a91905c10046e83e3ebba36377adc
Kimyen <kimyen0816@gmail.com>
2014-11-25 15:28:26 -0800

    update AccessRecordService

--- src/main/java/org/sagebionetworks/dashboard/service/AccessRecordService.java
+++ src/main/java/org/sagebionetworks/dashboard/service/AccessRecordService.java
@@ -1,10 +1,27 @@
 package org.sagebionetworks.dashboard.service;
 
+import javax.annotation.Resource;
+
+import org.sagebionetworks.dashboard.dao.AccessRecordDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service("accessRecordService")
 public class AccessRecordService {
 
+    private final Logger logger = LoggerFactory.getLogger(RawAccessRecordService.class);
+
+    @Resource
+    private AccessRecordDao accessRecordDao;
+
     public void update() {
-        // TODO Auto-generated method stub
+        // insert new raw records into access_record table
         
+        // vacuum
+        
+        // select 1000 null entityId records to update
+        // for each record, update the entityId field
     }
 
 }

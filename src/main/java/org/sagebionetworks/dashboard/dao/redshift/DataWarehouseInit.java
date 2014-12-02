b48c28a35bc8eb81fe55fc367d88a7837c7465e2
Kimyen <kimyen0816@gmail.com>
2014-12-02 14:44:13 -0800

    do not need to check if the table exists

--- src/main/java/org/sagebionetworks/dashboard/dao/redshift/DataWarehouseInit.java
+++ src/main/java/org/sagebionetworks/dashboard/dao/redshift/DataWarehouseInit.java
@@ -3,17 +3,12 @@ package org.sagebionetworks.dashboard.dao.redshift;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.PreparedStatementCallback;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import com.amazonaws.util.IOUtils;
@@ -21,7 +16,6 @@ import com.amazonaws.util.IOUtils;
 public class DataWarehouseInit {
 
     private final Logger logger = LoggerFactory.getLogger(DataWarehouseInit.class);
-    private final String TABLE_EXIST = "SELECT * FROM pg_tables WHERE tablename = ':tablename';";
 
     public DataWarehouseInit(NamedParameterJdbcTemplate dwTemplate) {
         createTable(dwTemplate, "/META-INF/spring/LogFileTable.sql", "log_file");
@@ -32,11 +26,6 @@ public class DataWarehouseInit {
     }
 
     private void createTable(NamedParameterJdbcTemplate dwTemplate, String path, String tablename) {
-
-        if (isExists(dwTemplate, tablename)) {
-            logger.info("Skip " + tablename + ". Reason: already existed.");
-            return;
-        }
 
         InputStream source = this.getClass().getResourceAsStream(path);
 
@@ -67,20 +56,4 @@ public class DataWarehouseInit {
         }
     }
 
-    private boolean isExists(NamedParameterJdbcTemplate dwTemplate, String tablename) {
-        Map<String, Object> parameter = new HashMap<>();
-        parameter.put("tablename", tablename);
-        List<String> result = dwTemplate.query(TABLE_EXIST, parameter, new TableRowMapper());
-        if (result.isEmpty()) {
-            return false;
-        }
-        return true;
-    }
-
-    private static class TableRowMapper implements RowMapper<String> {
-        @Override
-        public String mapRow(ResultSet rs, int rowNum) throws SQLException {
-            return rs.getString("tablename");
-        }
-    }
 }

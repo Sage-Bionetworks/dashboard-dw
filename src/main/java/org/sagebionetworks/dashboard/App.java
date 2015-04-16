cf3cf01e242684ed85a00912ec24fbe4261a6f88
eric-wu <eric.wu@sagebase.org>
2015-04-16 16:42:51 -0700

    Move config file out of META-INF

--- src/main/java/org/sagebionetworks/dashboard/App.java
+++ src/main/java/org/sagebionetworks/dashboard/App.java
@@ -9,7 +9,7 @@ public class App {
     public static void main(String[] args) {
 
         @SuppressWarnings("resource")
-        final ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/spring/app-context.xml");
+        final ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("/spring/app-context.xml");
         context.registerShutdownHook();
         context.start();
         LogFileDao logfileDao = context.getBean(LogFileDao.class);

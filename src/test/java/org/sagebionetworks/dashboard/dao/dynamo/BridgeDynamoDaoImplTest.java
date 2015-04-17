530eca1bc1f9fd9b03961416b4c97cd8582df411
eric-wu <eric.wu@sagebase.org>
2015-04-17 14:09:42 -0700

    Using the new Redshift JDBC driver

--- src/test/java/org/sagebionetworks/dashboard/dao/dynamo/BridgeDynamoDaoImplTest.java
+++ src/test/java/org/sagebionetworks/dashboard/dao/dynamo/BridgeDynamoDaoImplTest.java
@@ -8,10 +8,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.sagebionetworks.dashboard.dao.BridgeDynamoDao;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
 @ContextConfiguration("classpath:/spring/test-context.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners(listeners = {
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class})
 public class BridgeDynamoDaoImplTest {
 
     @Resource

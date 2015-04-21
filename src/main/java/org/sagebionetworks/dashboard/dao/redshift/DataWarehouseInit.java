329c3fe882e7ad1de94383eb3c0df49e01334abc
eric-wu <eric.wu@sagebase.org>
2015-04-20 17:00:00 -0700

    Annotate DataWarehouseInit as a component

--- src/main/java/org/sagebionetworks/dashboard/dao/redshift/DataWarehouseInit.java
+++ src/main/java/org/sagebionetworks/dashboard/dao/redshift/DataWarehouseInit.java
@@ -7,53 +7,47 @@ import java.sql.SQLException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import com.amazonaws.util.IOUtils;
 
+@Component("dwInit")
 public class DataWarehouseInit {
 
     private final Logger logger = LoggerFactory.getLogger(DataWarehouseInit.class);
+    private final NamedParameterJdbcTemplate dwTemplate;
 
+    @Autowired
     public DataWarehouseInit(NamedParameterJdbcTemplate dwTemplate) {
-        createTable(dwTemplate, "/spring/LogFileTable.sql");
-        createTable(dwTemplate, "/spring/RawAccessRecordTable.sql");
-        createTable(dwTemplate, "/spring/AccessRecordTable.sql");
-
+        this.dwTemplate = dwTemplate;
+        createTable("/spring/LogFileTable.sql");
+        createTable("/spring/RawAccessRecordTable.sql");
+        createTable("/spring/AccessRecordTable.sql");
         logger.info("Data warehouse initialzied.");
     }
 
-    private void createTable(NamedParameterJdbcTemplate dwTemplate, String path) {
-
-        InputStream source = this.getClass().getResourceAsStream(path);
-
-        if (source == null) {
-            logger.info("Failed to read the source from " + path
-                    + ". Data warehouse initiation failure.");
-            return;
-        }
-
-        String query = null;
-        try {
-            query = IOUtils.toString(source);
-        } catch (IOException e) {
-            logger.info("Failed to convert InputStream into String."
-                    + " Data warehouse initiation failure.");
-            return;
-        }
-
-        try {
+    private void createTable(String path) {
+        try (final InputStream source = this.getClass().getResourceAsStream(path)) {
+            if (source == null) {
+                logger.error("The source " + path
+                        + "is null. Data warehouse initiation has failed.");
+                return;
+            }
+            final String query = IOUtils.toString(source);
             dwTemplate.execute(query, new PreparedStatementCallback<Boolean>() {
                 @Override
                 public Boolean doInPreparedStatement(PreparedStatement ps)
                         throws SQLException, DataAccessException {
                     return ps.execute();
-                }});
-        } catch (DataAccessException e) {
-            logger.info("Failed to create table in file " + path);
+                }
+            });
+        } catch (DataAccessException | IOException e) {
+            logger.error("Data warehouse initiation failure.", e);
+            return;
         }
     }
-
 }

1529caaa37aa6106dff042a5057baa678eb8596e
eric.wu <eric.wu@sagebase.org>
2015-04-30 19:09:40 -0700

    Add mocked tests

--- src/test/java/org/sagebionetworks/dashboard/dao/redshift/BridgeImportDaoImplTest.java
+++ src/test/java/org/sagebionetworks/dashboard/dao/redshift/BridgeImportDaoImplTest.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+import org.sagebionetworks.dashboard.config.DwConfig;
 import org.sagebionetworks.dashboard.dao.BridgeImportDao;
 import org.sagebionetworks.dashboard.dao.DwDao;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -36,5 +37,27 @@ public class BridgeImportDaoImplTest {
         final String tableDropped = dao.dropTable(tableName, dateSuffix);
         assertEquals(fullTableName, tableDropped);
         verify(mockDwDao, times(1)).execute(startsWith("DROP TABLE "));
+    }
+
+    @Test
+    public void testCopyFromDynamo() {
+        BridgeImportDao dao = new BridgeImportDaoImpl();
+        DwConfig mockConfig = mock(DwConfig.class);
+        when(mockConfig.getBridgeAwsAccessKey()).thenReturn("accessKey");
+        when(mockConfig.getBridgeAwsSecretKey()).thenReturn("secretKey");
+        ReflectionTestUtils.setField(dao, "dwConfig", mockConfig);
+        DwDao mockDwDao = mock(DwDao.class);
+        ReflectionTestUtils.setField(dao, "dwDao", mockDwDao, DwDao.class);
+        dao.copyFromDynamo("dynamo", "redshift");
+        verify(mockDwDao, times(1)).execute(startsWith("COPY redshift FROM 'dynamodb://'"));
+    }
+
+    @Test
+    public void testUpdateView() {
+        BridgeImportDao dao = new BridgeImportDaoImpl();
+        DwDao mockDwDao = mock(DwDao.class);
+        ReflectionTestUtils.setField(dao, "dwDao", mockDwDao, DwDao.class);
+        dao.updateView("table", "snapshot");
+        verify(mockDwDao, times(1)).execute(startsWith("CREATE OR REPLACE VIEW "));
     }
 }

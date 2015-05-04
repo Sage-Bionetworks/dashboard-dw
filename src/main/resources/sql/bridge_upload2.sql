ebbded9bf349b548e4bd2c79c568519e92646609
eric.wu <eric.wu@sagebase.org>
2015-05-04 16:52:21 -0700

    Adjust fields according to real data

--- src/main/resources/sql/bridge_upload2.sql
+++ src/main/resources/sql/bridge_upload2.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS bridge_upload2_<dateSuffix> (
     uploadId                char(36)        not null,
-    uploadDate              timestamp       not null,
+    uploadDate              timestamp,
     filename                varchar(500),
     contentLength           int,
-    contentMd5              varchar(30),
-    contentType             varchar(30),
+    contentMd5              varchar(50),
+    contentType             varchar(50),
     healthCode              char(36)        not null,
-    status                  varchar(30)
+    status                  varchar(50)
 );

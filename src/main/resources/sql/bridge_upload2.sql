d68abcfb6cbb453611b9096b1660718197375a0b
eric.wu <eric.wu@sagebase.org>
2015-05-08 13:40:05 -0700

    Add sort keys

--- src/main/resources/sql/bridge_upload2.sql
+++ src/main/resources/sql/bridge_upload2.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS bridge_upload2_<dateSuffix> (
-    uploadId        char(36)        encode raw          not null,
-    uploadDate      timestamp       encode raw                  ,
-    filename        varchar(500)    encode lzo                  ,
-    contentLength   int             encode delta32k             ,
-    contentMd5      varchar(50)     encode raw                  ,
-    contentType     varchar(50)     encode bytedict             ,
-    healthCode      char(36)        encode raw          not null,
-    status          varchar(50)     encode bytedict
-);
+    uploadId        CHAR(36)        ENCODE RAW          NOT NULL,
+    uploadDate      DATE            ENCODE RAW                  ,
+    filename        VARCHAR(500)    ENCODE LZO                  ,
+    contentLength   INT             ENCODE DELTA32K             ,
+    contentMd5      VARCHAR(50)     ENCODE RAW                  ,
+    contentType     VARCHAR(50)     ENCODE BYTEDICT             ,
+    healthCode      CHAR(36)        ENCODE RAW          NOT NULL,
+    status          VARCHAR(50)     ENCODE BYTEDICT
+) SORTKEY (uploadDate, healthCode);

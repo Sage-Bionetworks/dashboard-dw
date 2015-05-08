de2dc83db845f84c53c448c743729b149db4a8eb
eric.wu <eric.wu@sagebase.org>
2015-05-08 12:51:24 -0700

    Specify compression encodings

--- src/main/resources/sql/bridge_upload2.sql
+++ src/main/resources/sql/bridge_upload2.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS bridge_upload2_<dateSuffix> (
-    uploadId                char(36)        not null,
-    uploadDate              timestamp,
-    filename                varchar(500),
-    contentLength           int,
-    contentMd5              varchar(50),
-    contentType             varchar(50),
-    healthCode              char(36)        not null,
-    status                  varchar(50)
+    uploadId        char(36)        encode raw          not null,
+    uploadDate      timestamp       encode raw                  ,
+    filename        varchar(500)    encode lzo                  ,
+    contentLength   int             encode delta32k             ,
+    contentMd5      varchar(50)     encode raw                  ,
+    contentType     varchar(50)     encode bytedict             ,
+    healthCode      char(36)        encode raw          not null,
+    status          varchar(50)     encode bytedict
 );

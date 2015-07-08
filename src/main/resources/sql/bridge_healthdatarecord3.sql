bd863064eebe246318af1ffe633402d21fbfeaf5
eric.wu <eric.wu@sagebase.org>
2015-07-08 10:30:27 -0700

    Relax the schema for HDR3

--- src/main/resources/sql/bridge_healthdatarecord3.sql
+++ src/main/resources/sql/bridge_healthdatarecord3.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS bridge_healthdatarecord3_<dateSuffix> (
-    id              CHAR(36)        ENCODE LZO          NOT NULL,
+    id              CHAR(36)        ENCODE RAW          NOT NULL,
     healthCode      CHAR(36)        ENCODE RAW          NOT NULL,
-    createdOn       TIMESTAMP       ENCODE DELTA32k     NOT NULL,
-    studyId         VARCHAR(50)     ENCODE BYTEDICT     NOT NULL,
+    createdOn       BIGINT          ENCODE DELTA32k             ,
+    studyId         VARCHAR(50)     ENCODE BYTEDICT             ,
     uploadId        CHAR(36)        ENCODE RAW          NOT NULL,
-    uploadDate      DATE            ENCODE RAW          NOT NULL,
-    schemaId        VARCHAR(250)    ENCODE LZO          NOT NULL,
-    schemaRevision  INT             ENCODE DELTA        NOT NULL,
-    data            VARCHAR(5000)   ENCODE LZO                  ,
-    metaData        VARCHAR(500)    ENCODE LZO                  ,
+    uploadDate      DATE            ENCODE RAW                  ,
+    schemaId        VARCHAR(250)    ENCODE LZO                  ,
+    schemaRevision  INT             ENCODE DELTA                ,
+    data            VARCHAR(10000)  ENCODE LZO                  ,
+    metaData        VARCHAR(5000)   ENCODE LZO                  ,
     version         INT             ENCODE DELTA
-) SORTKEY (studyId, uploadId, uploadDate, healthCode);
+) SORTKEY (studyId, uploadDate, schemaId, healthCode, uploadId);

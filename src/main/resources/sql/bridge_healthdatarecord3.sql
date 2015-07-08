62842b8a066d338169a740913e72ed3b6e9eeeb0
eric.wu <eric.wu@sagebase.org>
2015-07-08 11:43:46 -0700

    Add userSharingScope

--- src/main/resources/sql/bridge_healthdatarecord3.sql
+++ src/main/resources/sql/bridge_healthdatarecord3.sql
@@ -1,13 +1,20 @@
 CREATE TABLE IF NOT EXISTS bridge_healthdatarecord3_<dateSuffix> (
-    id              CHAR(36)        ENCODE RAW          NOT NULL,
-    healthCode      CHAR(36)        ENCODE RAW          NOT NULL,
-    createdOn       BIGINT          ENCODE DELTA32k             ,
-    studyId         VARCHAR(50)     ENCODE BYTEDICT             ,
-    uploadId        CHAR(36)        ENCODE RAW          NOT NULL,
-    uploadDate      DATE            ENCODE RAW                  ,
-    schemaId        VARCHAR(250)    ENCODE LZO                  ,
-    schemaRevision  INT             ENCODE DELTA                ,
-    data            VARCHAR(10000)  ENCODE LZO                  ,
-    metaData        VARCHAR(5000)   ENCODE LZO                  ,
-    version         INT             ENCODE DELTA
-) SORTKEY (studyId, uploadDate, schemaId, healthCode, uploadId);
+    id                  CHAR(36)        ENCODE RAW          NOT NULL,
+    healthCode          CHAR(36)        ENCODE RAW          NOT NULL,
+    createdOn           BIGINT          ENCODE DELTA32k             ,
+    studyId             VARCHAR(50)     ENCODE BYTEDICT             ,
+    uploadId            CHAR(36)        ENCODE RAW          NOT NULL,
+    uploadDate          DATE            ENCODE RAW                  ,
+    schemaId            VARCHAR(250)    ENCODE LZO                  ,
+    schemaRevision      INT             ENCODE DELTA                ,
+    data                VARCHAR(10000)  ENCODE LZO                  ,
+    metaData            VARCHAR(5000)   ENCODE LZO                  ,
+    userSharingScope    VARCHAR(500)    ENCODE BYTEDICT             ,
+    version             INT             ENCODE DELTA
+) SORTKEY (
+    studyId,
+    uploadDate,
+    schemaId,
+    healthCode,
+    uploadId
+);
